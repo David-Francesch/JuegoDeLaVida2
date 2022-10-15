7a3efb019ba8c2df86686bcb7d64bc916eccf185
David-Francesch <david.francesch.colomer@gmail.com>
2022-10-16 01:59:32 +0200

    Contar vecinos funciona, pero no se xq

--- src/App.java
+++ src/App.java
@@ -2,11 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         Mundo m1 = new Mundo();
         m1.MostrarMundo();
+
         int vecinos[][] = new int[8][8];
         for (int i = 0; i < vecinos.length; i++) {
             for (int j = 0; j < vecinos.length; j++) {
-                vecinos[i][j] = m1.ContarVecinos(i, j);
-                System.out.println(vecinos[i][j]+" ");
+                m1.ContarVecinos(i, j);
+                // System.out.println(vecinos[i][j]+" ");
             }
             System.out.println();
         }
--- src/Mundo.java
+++ src/Mundo.java
@@ -12,6 +12,10 @@ public class Mundo {
         }
     }
 
+    public Vida[][] getTablero() {
+        return tablero;
+    }
+
     public void MostrarMundo() {
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero.length; j++) {
@@ -21,142 +25,173 @@ public class Mundo {
         }
     }
 
-    public int ContarVecinos(int x, int y) {
-        int vecinos = 0;
+    // col , row : representing the cell you want to find neighbors to
+    public void ContarVecinos(int x, int y) {
 
-        if (x == 0 && y == 0) {
-            if (tablero[x+1][y].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-        }else if (x == tablero.length - 1 && y == 0) {
-            if (tablero[x-1][y].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-        }else if (y == tablero.length - 1 && y == 0) {
-            if (tablero[x][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y].getEstado() == 1) {
-                vecinos++;
-            }
-        }else if (x == tablero.length - 1 && y == tablero.length - 1) {
-            if (tablero[x-1][y].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y+1].getEstado() == 1) {
-                vecinos++;
+        // find all serouding cell by adding +/- 1 to col and row
+        for (int colNum = y - 1; colNum <= (y + 1); colNum += 1) {
+
+            for (int rowNum = x - 1; rowNum <= (x + 1); rowNum += 1) {
+
+                // if not the center cell
+                if (!((colNum == y) && (rowNum == x))) {
+
+                    // make sure it is within grid
+                    if (withinGrid(colNum, rowNum)) {
+                        int vida = tablero[rowNum][colNum].getEstado();
+                        System.out.println("Neighbor of " + y + " " + x + " - " + colNum + " " + rowNum + " Value: " + vida);
+                    }
+                }
             }
         }
-
-        if (x == 0 && y > 0 && y < tablero.length - 1) {
-            if (tablero[x][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-        }else if (y == 0 && x > 0 && x < tablero.length - 1) {
-            if (tablero[x-1][y].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y].getEstado() == 1) {
-                vecinos++;
-            }
-        }else if (x == tablero.length - 1 && y > 0 && y < tablero.length - 1) {
-            if (tablero[x][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-        }else if (y == tablero.length - 1 && x > 0 && x < tablero.length - 1) {
-            if (tablero[x-1][y].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y].getEstado() == 1) {
-                vecinos++;
-            }
-        }else{
-            if (tablero[x-1][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y+1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x+1][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y-1].getEstado() == 1) {
-                vecinos++;
-            }
-            if (tablero[x-1][y].getEstado() == 1) {
-                vecinos++;
-            }
-        }
-        return vecinos;
     }
+
+    // define if cell represented by colNum, rowNum is inside grid
+    // function used by neighbours()
+    public boolean withinGrid(int colNum, int rowNum) {
+
+        if ((colNum < 0) || (rowNum < 0)) {
+            return false; // false if row or col are negative
+        }
+        if ((colNum >= tablero.length -1) || (rowNum >= tablero.length -1)) {
+            return false; // false if row or col are > 75
+        }
+        return true;
+    }
+
+    // Ineficiente, no funciona
+
+    // if (x == 0 && y == 0) {
+    // if (tablero[x+1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }else if (x == tablero.length - 1 && y == 0) {
+    // if (tablero[x-1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }else if (y == tablero.length - 1 && y == 0) {
+    // if (tablero[x][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }else if (x == tablero.length - 1 && y == tablero.length - 1) {
+    // if (tablero[x-1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }
+
+    // if (x == 0 && y > 0 && y < tablero.length - 1) {
+    // if (tablero[x][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }else if (y == 0 && x > 0 && x < tablero.length - 1) {
+    // if (tablero[x-1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }else if (x == tablero.length - 1 && y > 0 && y < tablero.length - 1) {
+    // if (tablero[x][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }else if (y == tablero.length - 1 && x > 0 && x < tablero.length - 1) {
+    // if (tablero[x-1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }else{
+    // if (tablero[x-1][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y-1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x+1][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y+1].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // if (tablero[x-1][y].getEstado() == 1) {
+    // vecinos++;
+    // }
+    // }
 
 }
