009d2de75477290f7bed7fd4ce68d8bbd8d5f268
David-Francesch <david.francesch.colomer@gmail.com>
2022-10-16 01:05:18 +0200

    Primer try de contar vecinos(no chuta)

--- src/Mundo.java
+++ src/Mundo.java
@@ -1,10 +1,10 @@
 import java.lang.reflect.Array;
 
 public class Mundo {
-    Vida tablero[][] = new Vida[8][8]; 
+    Vida tablero[][] = new Vida[8][8];
     Vida copiatemp[][];
 
-    public Mundo(){
+    public Mundo() {
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero.length; j++) {
                 tablero[i][j] = new Vida();
@@ -15,10 +15,148 @@ public class Mundo {
     public void MostrarMundo() {
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero.length; j++) {
-                System.out.print(tablero[i][j].getEstado()+" ");
+                System.out.print(tablero[i][j].getEstado() + " ");
             }
             System.out.println();
         }
+    }
+
+    public int ContarVecinos(int x, int y) {
+        int vecinos = 0;
+
+        if (x == 0 && y == 0) {
+            if (tablero[x+1][y].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+        }else if (x == tablero.length - 1 && y == 0) {
+            if (tablero[x-1][y].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+        }else if (y == tablero.length - 1 && y == 0) {
+            if (tablero[x][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y].getEstado() == 1) {
+                vecinos++;
+            }
+        }else if (x == tablero.length - 1 && y == tablero.length - 1) {
+            if (tablero[x-1][y].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+        }
+
+        if (x == 0 && y > 0 && y < tablero.length - 1) {
+            if (tablero[x][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+        }else if (y == 0 && x > 0 && x < tablero.length - 1) {
+            if (tablero[x-1][y].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y].getEstado() == 1) {
+                vecinos++;
+            }
+        }else if (x == tablero.length - 1 && y > 0 && y < tablero.length - 1) {
+            if (tablero[x][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+        }else if (y == tablero.length - 1 && x > 0 && x < tablero.length - 1) {
+            if (tablero[x-1][y].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y].getEstado() == 1) {
+                vecinos++;
+            }
+        }else{
+            if (tablero[x-1][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y+1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x+1][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y-1].getEstado() == 1) {
+                vecinos++;
+            }
+            if (tablero[x-1][y].getEstado() == 1) {
+                vecinos++;
+            }
+        }
+        return vecinos;
     }
 
 }
